03eb621baafccaa4a19ce802e71fc5bda75c49f7
Shouko <h999342@gmail.com>
2014-05-28 13:05:36 +0800

    restrict to local access only

--- fetch_schoolbus.php
+++ fetch_schoolbus.php
@@ -14,8 +14,9 @@ function qMysql($str){
 }
 
 if($_SERVER['REMOTE_ADDR'] != '127.0.0.1'){
-  //die();
-//  echo "remote\n";
+  // restrict to local access only
+  header('HTTP/1.1 403 Forbidden');
+  die();
 }
 
 print_r($_POST);
@@ -24,8 +25,16 @@ if(isset($_POST['status'])&&isset($_POST['position'])){
   $status = json_decode($_POST['status']);
   $position = json_decode($_POST['position']);
 
+  // split into plate and driver
+  $status[0] = explode(',', $status[0]);
+  // remove km/h text
+  $status[1] = abs(explode(',', $status[1])[0]);
   print_r($status);
   print_r($position);
+
+  $query = "INSERT INTO `schoolbus`(`plate`,`driver`,`speed`,`utime`,`lat`,`long`) VALUES('".$status[0][0]."','".$status[0][1]."','".$status[1]."','".$status[2]."','".$position[0]."','".$position[1]."')";
+  // echo $query;
+  qMysql($query);
 }
 
 ?>
